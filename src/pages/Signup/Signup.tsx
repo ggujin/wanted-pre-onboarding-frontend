6ade06a9575b698f7ff91e916c5aa235fc91d21b
ggujin <yang97816@gmail.com>
2023-02-04 18:24:50 +0900

    style: apply signup page style

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -9,15 +9,33 @@ export function Signup() {
           <FcTodoList className={styles.icon} />
           회원가입
         </h2>
-        <div>
-          <input type="text" name="email" placeholder="이메일을 입력해주세요" />
+        <div className={styles.formItem}>
           <input
+            className={styles.input}
+            type="text"
+            name="email"
+            placeholder="이메일을 입력해주세요"
+            data-testid="email-input"
+          />
+        </div>
+        <div className={styles.formItem}>
+          <input
+            className={styles.input}
             type="password"
             name="password"
             placeholder="비밀번호를 입력해주세요"
+            data-testid="password-input"
           />
         </div>
-        <button type="submit">가입하기</button>
+        <div className={styles.formItem}>
+          <button
+            className={styles.submit}
+            type="submit"
+            data-testid="signup-button"
+          >
+            가입하기
+          </button>
+        </div>
       </form>
     </div>
   );
