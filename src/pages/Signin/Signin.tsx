2dd9989bc3b2310d3e33b33f452e821ed82a0e0a
ggujin <yang97816@gmail.com>
2023-02-07 22:00:34 +0900

    feat: apply signup validation

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -2,10 +2,24 @@ import { IoLogInOutline } from "react-icons/io5";
 
 import { CenterFormLayout } from "~/layouts/CenterFormLayout";
 import { Input } from "~/components/Input";
+import { useInput } from "~/Hooks/useInput";
 
 import styles from "./Signin.module.scss";
 
 export function Signin() {
+  const {
+    value: email,
+    onChange: handleEmailChange,
+    isValid: isEmailValid,
+  } = useInput({ validate: (email) => email.includes("@") });
+
+  const {
+    value: password,
+    onChange: handlePasswordChange,
+    isValid: isPasswordValid,
+  } = useInput({ validate: (password) => password.length >= 8 });
+
+  const isSubmitDisabled = !(isEmailValid && isPasswordValid);
   return (
     <CenterFormLayout>
       <form>
@@ -17,7 +31,9 @@ export function Signin() {
           <Input
             type="text"
             name="email"
+            value={email}
             placeholder="이메일을 입력해주세요"
+            onChange={handleEmailChange}
             data-testid="email-input"
           />
         </div>
@@ -25,7 +41,9 @@ export function Signin() {
           <Input
             type="password"
             name="password"
+            value={password}
             placeholder="비밀번호를 입력해주세요"
+            onChange={handlePasswordChange}
             data-testid="password-input"
           />
         </div>
@@ -33,6 +51,7 @@ export function Signin() {
           <button
             className={styles.submit}
             type="submit"
+            disabled={isSubmitDisabled}
             data-testid="signin-button"
           >
             로그인
