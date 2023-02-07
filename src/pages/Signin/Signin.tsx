e05c3b8a29a54c47d80efbbd829e549dcd93b2f3
ggujin <yang97816@gmail.com>
2023-02-07 19:30:59 +0900

    feat: add signin page

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,3 +1,44 @@
+import { IoLogInOutline } from "react-icons/io5";
+
+import { CenterFormLayout } from "~/layouts/CenterFormLayout";
+import { Input } from "~/components/Input";
+
+import styles from "./Signin.module.scss";
+
 export function Signin() {
-  return <div>Login</div>;
+  return (
+    <CenterFormLayout>
+      <form>
+        <h2 className={styles.formHeader}>
+          <IoLogInOutline className={styles.icon} />
+          로그인
+        </h2>
+        <div>
+          <Input
+            type="text"
+            name="email"
+            placeholder="이메일을 입력해주세요"
+            data-testid="email-input"
+          />
+        </div>
+        <div>
+          <Input
+            type="password"
+            name="password"
+            placeholder="비밀번호를 입력해주세요"
+            data-testid="password-input"
+          />
+        </div>
+        <div>
+          <button
+            className={styles.submit}
+            type="submit"
+            data-testid="signin-button"
+          >
+            로그인
+          </button>
+        </div>
+      </form>
+    </CenterFormLayout>
+  );
 }
