d81a503333530915a9168195de683a7c4c4023fa
ggujin <yang97816@gmail.com>
2023-02-15 23:48:10 +0900

    feat: add auth interceptor

--- src/libs/httpClient.ts
+++ src/libs/httpClient.ts
@@ -1,5 +1,12 @@
-import axios from "axios";
-
+import axios, { InternalAxiosRequestConfig } from "axios";
 export const httpClient = axios.create({
   baseURL: "https://pre-onboarding-selection-task.shop",
+});
+
+httpClient.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (!accessToken) return config;
+
+  config.headers.set("Authorization", `Bearer ${accessToken}`);
+  return config;
 });
